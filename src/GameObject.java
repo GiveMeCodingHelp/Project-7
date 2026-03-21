163a3bf124f5d6282fcf9c10ddc36af121355099
GiveMeCodingHelp <gomberg.main.user@gmail.com>
2026-03-20 22:31:31 -0700

    adding GameObject methods

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,4 +1,5 @@
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameObject {
     private double x;
@@ -6,4 +7,17 @@ public class GameObject {
     private int width;
     private int height;
     private Texture image;
+
+    public GameObject(double x, double y, int width, int height, String imagePath) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = new Texture(imagePath);
+    }
+
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(image, (int) x, (int) y, width, height);
+    }
 }
