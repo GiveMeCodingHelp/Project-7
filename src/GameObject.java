75c4c91dd5c1a3b831246491ffff07c5f807c8bb
GiveMeCodingHelp <gomberg.main.user@gmail.com>
2026-03-20 22:26:44 -0700

    Actually added assets folder this time and also GameObject class

---
+++ src/GameObject.java
@@ -0,0 +1,9 @@
+import com.badlogic.gdx.graphics.Texture;
+
+public class GameObject {
+    private double x;
+    private double y;
+    private int width;
+    private int height;
+    private Texture image;
+}
