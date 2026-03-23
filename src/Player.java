be7d07a892174d00dfcb6bd8f4146e042737d94d
GiveMeCodingHelp <gomberg.main.user@gmail.com>
2026-03-23 00:26:47 -0700

    added multi thread parrying logic (a lil google ai search goes a long way)

--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,30 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Player extends GameObject{
-    private boolean isParry = false;
+    private volatile boolean isParry = false;
+    private volatile boolean cooldown = false;
+    private ScheduledExecutorService scheduler;
     //creates player and centers to bottom of screen
     public Player(){
         super(0,0,100,100,"assets/fish_blue.png");
         super.x = (double) (Launcher.gameWidth - width) / 2;
+        scheduler = Executors.newScheduledThreadPool(1);
     }
 
     @Override
     public void move(double deltaTime) {
-        super.move(deltaTime);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F) && !cooldown){
+            isParry = true;
+            cooldown = true;
+            scheduler.schedule(() -> {
+                isParry = false;
+                cooldown = false;
+            }, 3, TimeUnit.SECONDS);
+        }
+        System.out.println(isParry);
     }
 }
